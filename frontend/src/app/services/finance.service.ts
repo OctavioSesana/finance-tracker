1723adc70afa7d6c4bd2e33668831cb0a8bc0351
OctavioSesana <osesana@frro.utn.edu.ar>
2026-03-23 13:25:30 -0300

    fix: dashboard service api

--- frontend/src/app/services/finance.service.ts
+++ frontend/src/app/services/finance.service.ts
@@ -1,13 +1,14 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable, map } from 'rxjs';
+import { environment } from '../../environments/environment.prod';
 
 @Injectable({
   providedIn: 'root'
 })
 export class FinanceService {
 
-  private apiUrl = 'http://localhost:3000/api/transactions';
+  private apiUrl = `${environment.apiUrl}/api/transactions`;
 
   constructor(private http: HttpClient) {}
 
